88c06bf2aeaa65406c67fc167c3e416bc9443341
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2023-01-10 14:36:29 -0300

    Propriedades

--- reactapp/src/pages/home/index.jsx
+++ reactapp/src/pages/home/index.jsx
@@ -1,4 +1,5 @@
 import './style.css'
+import {Card} from '../../components/Card';
 
 export function Home() {  
   return (
@@ -6,6 +7,9 @@ export function Home() {
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+      <Card name="Bernardo" time="10:55:22"/>
+      <Card name="Mariana" time="04:20:00"/>           
+      <Card name="Alef" time="12:20:00"/>           
     </div>
   )
 }
